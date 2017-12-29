b2eaeaa137b7a4f659ebda9a75bff598b46a3225
Patrick Michelberger <patrick.michelberger@tum.de>
2017-12-29 16:17:04 +0100

    Implement initial prediction modal

--- superset/assets/visualizations/odm/prediction_screengrid.jsx
+++ superset/assets/visualizations/odm/prediction_screengrid.jsx
@@ -1,44 +1,101 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ScreenGridLayer } from 'deck.gl';
+import {ScreenGridLayer} from 'deck.gl';
+import {Modal} from 'react-bootstrap';
 
 import DeckGLContainer from '../deckgl/DeckGLContainer';
 
-function PredictionScreenGridLayer(slice, payload, setControlValue) {
-  console.log("slice:", slice);
-  console.log("payload: ", payload);
-  const fd = slice.formData;
-  const c = fd.color_picker;
-  const data = payload.data.features.map(d => ({
-    ...d,
-    color: [c.r, c.g, c.b, 255 * c.a],
-  }));
+class PredictionScreenGrid extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: true,
+      info: {
+        object: null,
+        index: null
+      }
+    };
 
-  const viewport = {
-    ...fd.viewport,
-    width: slice.width(),
-    height: slice.height(),
+    this.toggleModal = this.toggleModal.bind(this);
+    this.onEnterModal = this.onEnterModal.bind(this);
+  }
+
+  toggleModal(info) {
+    console.log("toggleModal: info: ", info);
+    this.setState({showModal: !this.state.showModal, info: info});
+  }
+
+  onEnterModal() {
+    console.log("on enter modal");
   };
-  // Passing a layer creator function instead of a layer since the
-  // layer needs to be regenerated at each render
-  const layer = () => new ScreenGridLayer({
-    id: `screengrid-layer-${slice.containerId}`,
-    data,
-    pickable: true,
-    cellSizePixels: fd.grid_size,
-    minColor: [c.r, c.g, c.b, 0],
-    maxColor: [c.r, c.g, c.b, 255 * c.a],
-    outline: false,
-    getWeight: d => d.weight || 0,
-  });
+
+  render() {
+    const {slice, payload, setControlValue} = this.props;
+    const {info} = this.state;
+
+    const fd = slice.formData;
+    const c = fd.color_picker;
+    const data = payload.data.features.map(d => ({
+      ...d,
+      color: [c.r, c.g, c.b, 255 * c.a],
+    }));
+
+    const viewport = {
+      ...fd.viewport,
+      width: slice.width(),
+      height: slice.height(),
+    };
+
+    // Passing a layer creator function instead of a layer since the
+    // layer needs to be regenerated at each render
+    const layer = () => new ScreenGridLayer({
+      id: `screengrid-layer-${slice.containerId}`,
+      data,
+      pickable: true,
+      cellSizePixels: fd.grid_size,
+      minColor: [c.r, c.g, c.b, 0],
+      maxColor: [c.r, c.g, c.b, 255 * c.a],
+      outline: false,
+      getWeight: d => d.weight || 0,
+      onClick: info => this.toggleModal(info)
+    });
+
+    return (
+        <div>
+          <DeckGLContainer
+              mapboxApiAccessToken={payload.data.mapboxApiKey}
+              viewport={viewport}
+              layers={[layer]}
+              mapStyle={fd.mapbox_style}
+              setControlValue={setControlValue}
+          />
+          {info.object && info.object.position ? <Modal
+                  show={this.state.showModal}
+                  onHide={this.toggleModal}
+                  onEnter={this.onEnterModal}
+                  bsSize="lg"
+              >
+                <Modal.Header closeButton>
+                  <Modal.Title>Demand Prediction for
+                    Lat: {info.object.position[0]} |
+                    Lon: {info.object.position[1]}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <div>Weight: {info.object.weight}</div>
+                </Modal.Body>
+              </Modal>
+              : null}
+        </div>
+    )
+  }
+}
+
+function PredictionScreenGridLayer(slice, payload, setControlValue) {
+
   ReactDOM.render(
-      <DeckGLContainer
-          mapboxApiAccessToken={payload.data.mapboxApiKey}
-          viewport={viewport}
-          layers={[layer]}
-          mapStyle={fd.mapbox_style}
-          setControlValue={setControlValue}
-      />,
+      <PredictionScreenGrid slice={slice}
+                            payload={payload}
+                            setControlValue={setControlValue}/>,
       document.getElementById(slice.containerId),
   );
 }
